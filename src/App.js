dbe71c77dccd3dc5a86db696141decbdd418b0bd
LuizaTaysumova <36314072+LuizaTaysumova@users.noreply.github.com>
2019-02-19 10:20:59 +0300

    Добавлена валидация полей и выбор города

--- src/App.js
+++ src/App.js
@@ -1,28 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Form from './Form.js';
+import Table from './Table.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            membersData: [],
+            cities: ["Москва", "Санкт-Петербург", "Екатеринбург", "Нижний Новгород", "Новосибирск", "Казань", "Челябинск", "Омск", "Самара", "Ростов-на-Дону"]
+        }
+    }
+
+    componentDidMount() {
+        this.hydrateStateWithLocalStorage();
+
+        // add event listener to save state to localStorage
+        // when user leaves/refreshes the page
+        window.addEventListener(
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
+        );
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
+        );
+
+        // saves if component has a chance to unmount
+        this.saveStateToLocalStorage();
+    }
+
+    hydrateStateWithLocalStorage() {
+        // for all items in state
+        for (let key in this.state) {
+            // if the key exists in localStorage
+            if (localStorage.hasOwnProperty(key)) {
+                // get the key's value from localStorage
+                let value = localStorage.getItem(key);
+
+                // parse the localStorage string and setState
+                try {
+                    value = JSON.parse(value);
+                    this.setState({[key]: value});
+                } catch (e) {
+                    // handle empty string
+                    this.setState({[key]: value});
+                }
+            }
+        }
+    };
+
+    saveStateToLocalStorage() {
+        // for every item in React state
+        for (let key in this.state) {
+            // save to localStorage
+            localStorage.setItem(key, JSON.stringify(this.state[key]));
+        }
+    }
+
+    handleSubmit = memberData => {
+        this.setState({membersData: [...this.state.membersData, memberData]});
+    }
+
+    editAd = index => {
+        const {membersData} = this.state;
+        const formTitle = document.getElementsByTagName('input')[0];
+        const formCity = document.getElementsByTagName('input')[1];
+        const formNumber = document.getElementsByTagName('input')[2];
+        formTitle.value = membersData[index].memberName;
+        formCity.value = membersData[index].memberCity;
+        formNumber.value = membersData[index].memberPhone;
+        console.log(membersData[index]);
+        formTitle.focus();
+    }
+
+    removeMember = index => {
+        const {membersData} = this.state;
+
+        this.setState({
+            membersData: membersData.filter((memberData, i) => {
+                return i !== index;
+            })
+        });
+
+        console.log(this.cities);
+    }
+
+    render() {
+        const {membersData, cities} = this.state;
+        return (
+            <div className="container">
+                <Form handleSubmit={this.handleSubmit}
+                      membersData={membersData}
+                      cities={cities}
+                />
+                <Table editMemberInfo={this.editMemberInfo}
+                       removeMember={this.removeMember}
+                       membersData={membersData}/>
+            </div>
+        );
+    }
 }
 
 export default App;
